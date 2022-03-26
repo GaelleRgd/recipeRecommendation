2447cbb19817503e2a610f86aea81a28aab0cd7e
GaelleRgd <77722509+GaelleRgd@users.noreply.github.com>
2022-03-27 00:40:22 +0100

    Update application.py

--- application.py
+++ application.py
@@ -3,9 +3,17 @@ from flask import Flask, jsonify, request, render_template
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods = ['GET'])
 def hello():
-    return "Hello World!"
+    if( request.method == 'GET'):
+        data = {
+            "status" : "ok",
+            "message" : "Hello World !"
+        }
+        return jsonify(data)
+    else : 
+        return jsonify({})
+
 
 if __name__ == "__main__":
     app.run()
